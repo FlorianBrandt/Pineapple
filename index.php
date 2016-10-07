c12f9f6901ea75585b6fb8526cc1ea6a48f47259
Florian Brandt <business@fbrandt.space>
2016-10-07 19:31:32 +0200

    Base Site Template

---
+++ index.php
@@ -0,0 +1,46 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="de">
+    <head>
+        <meta charset="utf-8">
+        <title>Pineapple &middot; Admin</title>
+        <link rel="stylesheet" type="text/css" href="assets/styles/main.css">
+    </head>
+    <body>
+        <header>
+            <div class="branding">Pineapple</div>
+            <nav>
+                <ul>
+                    <li>
+                        <a href="#"><i class="fa fa-bars" aria-hidden="true"></i></a>
+                    </li>
+                    <li>
+                        <a href="#"><i class="fa fa-user" aria-hidden="true"></i></a>
+                    </li>
+                    <li>
+                        <a href="#"><i class="fa fa-cog" aria-hidden="true"></i></a>
+                    </li>
+                    <li>
+                        <a href="#"><i class="fa fa-wifi" aria-hidden="true"></i></a>
+                    </li>
+                    <!-------------------------------------------------------------->
+
+                    <li class="right">
+                        <a href="#"><i class="fa fa-lock" aria-hidden="true"></i></a>
+                    </li>
+                    <li class="right">
+                        <a href="#"><i class="fa fa-search" aria-hidden="true"></i></a>
+                    </li>
+                    <li class="right">
+                        <a href="#"><i class="fa fa-refresh" aria-hidden="true"></i></a>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+        <main>
+
+        </main>
+    </body>
+</html>
